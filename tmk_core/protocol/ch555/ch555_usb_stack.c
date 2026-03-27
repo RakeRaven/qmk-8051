a4ecb49defd2036413121b00a929de1dc8160ab0
TechnoClasher <shreemoym720@gmail.com>
2026-03-27 19:24:06 +0530

    fix: resolve raw_hid_receive pointer type conflict for SDCC

--- tmk_core/protocol/ch555/ch555_usb_stack.c
+++ tmk_core/protocol/ch555/ch555_usb_stack.c
@@ -305,7 +305,7 @@ void raw_hid_send(uint8_t *data, uint8_t length) {
 #ifndef VIA_ENABLE
 #pragma save
 //#pragma stackauto
-void raw_hid_receive(__xdata uint8_t *data, uint8_t length) {
+void raw_hid_receive(uint8_t *data, uint8_t length) {
     // Users should #include "raw_hid.h" in their own code
     // and implement this function there. Leave this as weak linkage
     // so users can opt to not handle data coming in.
