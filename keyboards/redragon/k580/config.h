a5454f2771ceebdc3fbd9b90da8b09e297a3bdc1
TechnoClasher <shreemoym720@gmail.com>
2026-04-02 23:19:37 +0530

    feat(k580): 8-row matrix, macro/media keys, encoder, extrakeys

--- keyboards/redragon/k580/config.h
+++ keyboards/redragon/k580/config.h
@@ -1,14 +1,14 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 
 #pragma once
-#define EEPROM_SIZE 128
+#define EEPROM_SIZE 127
 /* Matrix config */
 
-#define MATRIX_ROWS 7 //Keypu 0 - 6
-#define MATRIX_COLS 18 //Com 16 - 31
+#define MATRIX_ROWS 8  // Keypu 0 - 7
+#define MATRIX_COLS 18 // Com 16 - 33
 
-#define MATRIX_ROW_PINS { PORT4_0, PORT4_1, PORT4_2, PORT4_3, PORT4_4, PORT4_5, PORT4_6}
-#define MATRIX_COL_PINS { PORT0_0, PORT0_1, PORT0_2, PORT0_3, PORT0_4, PORT0_5, PORT0_6, PORT0_7, PORT3_0, PORT3_1,  PORT3_2, PORT3_3, PORT3_4, PORT3_5, PORT3_6, PORT3_7, PORT7_0, PORT7_1}
+#define MATRIX_ROW_PINS {PORT4_0, PORT4_1, PORT4_2, PORT4_3, PORT4_4, PORT4_5, PORT4_6, PORT4_7}
+#define MATRIX_COL_PINS {PORT0_0, PORT0_1, PORT0_2, PORT0_3, PORT0_4, PORT0_5, PORT0_6, PORT0_7, PORT3_0, PORT3_1, PORT3_2, PORT3_3, PORT3_4, PORT3_5, PORT3_6, PORT3_7, PORT7_0, PORT7_1}
 
 #define DIODE_DIRECTION ROW2COL
 
@@ -17,8 +17,8 @@
 // Try combinations: (PORT5_4, PORT5_5), (PORT5_4, PORT5_6), (PORT5_5, PORT5_6), etc.
 // Swap A and B if direction is reversed.
 #define NUM_ENCODERS 1
-#define ENCODERS_PAD_A { PORT2_7 }
-#define ENCODERS_PAD_B { PORT2_0 }
+#define ENCODERS_PAD_A {PORT5_5}
+#define ENCODERS_PAD_B {PORT5_6}
 #define ENCODER_RESOLUTION 2
 
 #define NO_PRINT
@@ -26,23 +26,28 @@
 #define ACTION_DEBUG
 
 // for ch552, the code size is too small:
-//#define NO_ACTION_LAYER
-//#define NO_ACTION_ONESHOT
-//#define NO_ACTION_TAPPING
+// #define NO_ACTION_LAYER
+// #define NO_ACTION_ONESHOT
+// #define NO_ACTION_TAPPING
 
-//#define HOLD_ON_OTHER_KEY_PRESS
+// #define HOLD_ON_OTHER_KEY_PRESS
 
 /* Bootmagic: hold this key at power-on to jump to bootloader */
 /* Row 0, Col 0 = Esc key on the K580 (confirmed from EVT demo key table) */
-#define BOOTMAGIC_LITE_ROW    0
+#define BOOTMAGIC_LITE_ROW 0
 #define BOOTMAGIC_LITE_COLUMN 0
 
 #define USB_MAX_POWER_CONSUMPTION 100
 
+/* RGB Matrix */
+//#define RGB_MATRIX_LED_COUNT 104 /* 104 physical keys; DMA buffer is 18×6=108 internally */
+//#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 200
+//#define RGB_MATRIX_DEFAULT_VAL RGB_MATRIX_MAXIMUM_BRIGHTNESS
+
 /* Indicator LEDs — active high (LED on when pin is driven high) */
-#define LED_NUM_LOCK_PIN    PORT5_3
-#define LED_CAPS_LOCK_PIN   PORT5_2
+#define LED_NUM_LOCK_PIN PORT5_3
+#define LED_CAPS_LOCK_PIN PORT5_2
 #define LED_SCROLL_LOCK_PIN PORT5_1
-#define LED_PIN_ON_STATE    1
+#define LED_PIN_ON_STATE 1
 /* PORT5_0 = Win Lock LED — pin defined for future use, logic not implemented yet */
-#define LED_WIN_LOCK_PIN    PORT5_0
+#define LED_WIN_LOCK_PIN PORT5_0
--- keyboards/redragon/k580/keymaps/default/keymap.c
+++ keyboards/redragon/k580/keymaps/default/keymap.c
@@ -1,27 +1,134 @@
 #include QMK_KEYBOARD_H
 
+// Macro Keys (for default keymap)
+enum {
+  U_G1 = SAFE_RANGE,
+  U_G2,
+  U_G3,
+  U_G4,
+  U_G5,
+  U_REC,
+  U_VOLBRI // Volume/Brightness toggle
+};
+bool BRI = false; // Keep track of volume/brightness for the encoder
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //COM is Row, KeyPu is Column
 	[0] = LAYOUT_default(
-		/*COM16*/	/*COM17*/	/*COM18*/	/*COM19*/	/*COM20*/	/*COM21*/	/*COM22*/	/*COM23*/	/*COM24*/	/*COM25*/ /*COM25*/	/*COM27*/	/*COM28*/	/*COM29*/	/*COM30*/	/*COM31*/
-        KC_ESC, KC_F1,   	KC_F2,   	KC_F3,   	KC_F4,   	KC_F5,   	KC_F6,   	KC_F7,   	KC_F8,    KC_F9,    KC_F10,  KC_F11,  	KC_F12,  	KC_PSCR, 	KC_SCRL, 	KC_PAUS,    KC_NUM,  KC_PSLS,
-        KC_GRV, KC_1,    	KC_2,    	KC_3,    	KC_4,    	KC_5,    	KC_6,    	KC_7,    	KC_8,     KC_9,    	KC_0,    KC_MINS, 	KC_EQL,  	KC_BSPC, 	KC_INS,  	KC_HOME,    KC_PGUP, KC_PAST,
-        KC_TAB, KC_Q,    	KC_W,    	KC_E,    	KC_R,    	KC_T,    	KC_Y,    	KC_U,    	KC_I,     KC_O,    	KC_P,    KC_LBRC, 	KC_RBRC, 	KC_BSLS, 	KC_DEL,  	KC_END,     KC_PGDN, KC_PMNS,
-        KC_CAPS, KC_A,   	KC_S,    	KC_D,    	KC_F,    	KC_G,    	KC_H,    	KC_J,    	KC_K,     KC_L,    	KC_SCLN, KC_QUOT, 	            KC_ENT,     KC_P7,      KC_P8,      KC_P9,   KC_PPLS,
-        KC_LSFT, KC_Z,      KC_X,    	KC_C,    	KC_V,    	KC_B,    	KC_N,    	KC_M,    	KC_COMM,  KC_DOT,  	KC_SLSH,             	        KC_RSFT,    KC_UP,      KC_P4,      KC_P5,   KC_P6,
-        KC_LCTL, KC_LGUI,   KC_LALT, 	KC_SPC,  	KC_RALT, 	KC_RGUI, 	MO(0),   	KC_RCTL,    KC_LEFT,  KC_DOWN,  KC_RIGHT,KC_P0, 	KC_PDOT, 	KC_P1,  	KC_P2,      KC_P3,      KC_PENT,
-                KC_NUM,   KC_PENT,                  KC_P5,      KC_PGUP,      KC_PGUP,    KC_PGDN,    KC_P9,    KC_PSLS,  KC_PPLS, KC_PMNS,   KC_PAST),
+		/*COM16*/	  /*COM17*/	  /*COM18*/	/*COM19*/	/*COM20*/	/*COM21*/	/*COM22*/	/*COM23*/	/*COM24*/	/*COM25*/ /*COM25*/	/*COM27*/	/*COM28*/	/*COM29*/	/*COM30*/	/*COM31*/   /*COM32*/	/*COM33*/
+        KC_ESC,   KC_F1,   	KC_F2,   	KC_F3,   	KC_F4,   	KC_F5,   	KC_F6,   	KC_F7,   	KC_F8,    KC_F9,    KC_F10,   KC_F11,  	KC_F12,  	KC_PSCR, 	KC_SCRL, 	KC_PAUS,    KC_NUM,  KC_PSLS,
+        KC_GRV,   KC_1,    	KC_2,    	KC_3,    	KC_4,    	KC_5,    	KC_6,    	KC_7,    	KC_8,     KC_9,    	KC_0,     KC_MINS, 	KC_EQL,  	KC_BSPC, 	KC_INS,  	KC_HOME,    KC_PGUP, KC_PAST,
+        KC_TAB,   KC_Q,    	KC_W,    	KC_E,    	KC_R,    	KC_T,    	KC_Y,    	KC_U,    	KC_I,     KC_O,    	KC_P,     KC_LBRC, 	KC_RBRC, 	KC_BSLS, 	KC_DEL,  	KC_END,     KC_PGDN, KC_PMNS,
+        KC_CAPS,  KC_A,   	KC_S,    	KC_D,    	KC_F,    	KC_G,    	KC_H,    	KC_J,    	KC_K,     KC_L,    	KC_SCLN,  KC_QUOT, 	          KC_ENT,   KC_P7,    KC_P8,      KC_P9,   KC_PPLS,
+        KC_LSFT,  KC_Z,     KC_X,    	KC_C,    	KC_V,    	KC_B,    	KC_N,    	KC_M,    	KC_COMM,  KC_DOT,  	KC_SLSH,             	        KC_RSFT,  KC_UP,    KC_P4,      KC_P5,   KC_P6,
+        KC_LCTL,  KC_LGUI,  KC_LALT, 	KC_SPC,  	KC_RALT, 	KC_RGUI, 	MO(0),   	KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RIGHT, KC_P0, 	  KC_PDOT, 	KC_P1,  	KC_P2,    KC_P3,      KC_PENT,
+        /* row 6 — macro keys */                                    U_G1,     U_G2,     U_G3,      U_G4,    U_G5,     U_REC,
+        /* row 7 — media keys */                                                                                                U_VOLBRI, KC_MPRV, KC_MPLY, KC_MNXT),
 };
+/*
+/* Macro LED support
+                    {G1, G2, G3, G4, G5, REC} */
+//int macro_state[] = {0,  0,  0,  0,  0,  0  };
+
+// Map Macro state to RGB channels of LED indices 77 and 78
+/*
+void set_macro_leds(int state[]) {
+    rgb_matrix_set_color(118, state[2]*255, state[1]*255, state[0]*255);
+    rgb_matrix_set_color(119, state[5]*255, state[4]*255, state[3]*255);
+}
+
+void macro_led_toggle(int i) {
+    macro_state[i] = 1-macro_state[i];
+    set_macro_leds(macro_state);
+}
+
+void macro_led_on(int i) {
+    macro_state[i] = 1;
+    set_macro_leds(macro_state);
+}
+
+void macro_led_off(int i) {
+    macro_state[i] = 0;
+    set_macro_leds(macro_state);
+}
+
+void macro_led_clear(void) {
+    for (int i = 0; i < 6; i++){
+        macro_state[i] = 0;
+    }
+    set_macro_leds(macro_state);
+}
+
+*/
+// Macro support (for default keymap)
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+    case U_VOLBRI:
+        if (record->event.pressed) {
+            // switch volume/brightness for encoder
+            BRI = !BRI;
+        } else {
+            // on release
+        }
+        break;/*
+    case U_G1:
+        if (record->event.pressed) {
+            macro_led_toggle(0);
+        }
+        break;
+    case U_G2:
+        if (record->event.pressed) {
+            macro_led_toggle(1);
+        }
+        break;
+    case U_G3:
+        if (record->event.pressed) {
+            macro_led_toggle(2);
+        }
+        break;
+    case U_G4:
+        if (record->event.pressed) {
+            macro_led_toggle(3);
+        }
+        break;
+    case U_G5:
+        if (record->event.pressed) {
+            macro_led_toggle(4);
+        }
+        break;
+    case U_REC:
+        if (record->event.pressed) {
+            macro_led_toggle(5);
+        }
+        break;*/
+    }
+    return true;
+};
+
+//encoder support
 
 
 #ifdef ENCODER_MAP_ENABLE
-const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [1] = { ENCODER_CCW_CW(KC_WH_D, KC_WH_U) },
-    [2] = { ENCODER_CCW_CW(KC_BRID, KC_BRIU) },
-};
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 0) { // First encoder 
+        if (clockwise) {
+            if(BRI){
+                rgblight_increase_val_noeeprom();
+            }else{
+                tap_code(KC_VOLU);
+            }
+        } else {
+            if(BRI){
+                rgblight_decrease_val_noeeprom();
+            }else{
+                tap_code(KC_VOLD);
+            }
+        }
+    }
+    return true;
+}
 #endif
-
 
 
 void keyboard_post_init_user(void) {}
